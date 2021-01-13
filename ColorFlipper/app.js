22abeb1d97f726ae57fc9597244453dafd474937
Solomon Asiedu <asiedusolo.sa@gmail.com>
2021-01-13 10:12:27 +0200

    simple color change functionality refactored

--- ColorFlipper/app.js
+++ ColorFlipper/app.js
@@ -1,12 +1,25 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const body = document.querySelector('body');
-const colorText = document.getElementsByClassName('color');
+// const colorText = document.getElementsByClassName('color');
+const colorText = document.querySelector('.color');
+const btn = document.getElementById('btn');
 
-function changeSimpleColor(){
-    var randNum = Math.floor(Math.random() * colors.length);
+
+// function changeSimpleColor(){
+//     var randNum = Math.floor(Math.random() * colors.length);
+//     body.style.backgroundColor = colors[randNum];
+//     colorText[0].textContent = colors[randNum];
+// }
+
+btn.addEventListener("click", function(){
+    var randNum = getRandomNumber();
     body.style.backgroundColor = colors[randNum];
-    colorText[0].textContent = colors[randNum];
+    colorText.textContent = colors[randNum];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
 }
 
 
