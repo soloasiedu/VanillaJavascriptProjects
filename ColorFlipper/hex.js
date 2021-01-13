4f4c1716ec65bcc255de54f2389e474f75f9d268
Solomon Asiedu <asiedusolo.sa@gmail.com>
2021-01-13 09:22:13 +0200

    hex color change functionality added

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,1 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const body = document.querySelector('body');
+const colorText = document.getElementsByClassName('color');
+
+function changeHexColor(){
+    var hexCode = "#";
+
+    for(let i = 0; i < 6; i++){
+        hexCode += hex[Math.floor(Math.random() * hex.length)]
+    }
+    body.style.backgroundColor = hexCode;
+    colorText[0].textContent = hexCode;
+
+}
