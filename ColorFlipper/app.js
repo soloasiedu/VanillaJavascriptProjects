7c5f807e30cfdc7c35e483ed5122161a21061790
Solomon Asiedu <asiedusolo.sa@gmail.com>
2021-01-13 09:14:54 +0200

    simple color changes functionality added

--- ColorFlipper/app.js
+++ ColorFlipper/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const body = document.querySelector('body');
+const colorText = document.getElementsByClassName('color');
+
+function changeSimpleColor(){
+    var randNum = Math.floor(Math.random() * colors.length);
+    body.style.backgroundColor = colors[randNum];
+    colorText[0].textContent = colors[randNum];
+}
+
+
