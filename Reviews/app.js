224c152f3887d2663c19adf05ce415efa3e167da
Solomon Asiedu <asiedusolo.sa@gmail.com>
2021-01-14 08:41:20 +0200

    other functionalities added

--- Reviews/app.js
+++ Reviews/app.js
@@ -42,11 +42,64 @@ const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randBtn = document.querySelector('.random-btn');
 
+const image = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const paragraph = document.getElementById("info");
 
-randBtn.addEventListener("click", function(){
+let currentItem = 0;
+window.addEventListener('DOMContentLoaded', function(){
+  showPerson(currentItem);
+});
+
+
+function showPerson(person){
+  const item = reviews[person];
+  image.src = item.src;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  paragraph.textContent = item.text;
+}
+
+randBtn.addEventListener("click", function () {
   var randNum = Math.floor(Math.random() * reviews.length);
-  document.getElementById("person-img").src = reviews[randNum].img;
-  document.getElementById("author").innerHTML = reviews[randNum].name;
-  document.getElementById("job").innerHTML = reviews[randNum].job;
-  document.getElementById("info").innerHTML = reviews[randNum].text;
+  image.src = reviews[randNum].img;
+  author.innerHTML = reviews[randNum].name;
+  job.innerHTML = reviews[randNum].job;
+  paragraph.innerHTML = reviews[randNum].text;
 })
+
+nextBtn.addEventListener("click", function () {
+  // var currentPersonId = searchId();
+  // var nexPersonId = currentPersonId + 1;
+  // if (nexPersonId === 5) {
+  //   nexPersonId = 1;
+  // }
+  // image.src = reviews[nexPersonId - 1].src;
+  // author.innerHTML = reviews[nexPersonId - 1].name;
+  // job.innerHTML = reviews[nexPersonId - 1].job;
+  // paragraph.innerHTML = reviews[nexPersonId - 1].text;
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+})
+
+prevBtn.addEventListener("click", function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+})
+
+
+// function searchId() {
+//   for (let i = 0; i < reviews.length; i++) {
+//     if (author.innerHTML === reviews[i].name) {
+//       return reviews[i].id;
+//     }
+//   }
+
+// }
