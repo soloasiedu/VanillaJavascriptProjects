1db9a421d32a5d40a02c8341a258f3ae3b79ea84
Solomon Asiedu <asiedusolo.sa@gmail.com>
2021-01-26 17:25:33 +0200

    adding functionality to clear all the grocery items

--- Grocery/app.js
+++ Grocery/app.js
@@ -14,6 +14,8 @@ let editID = "";
 // ****** EVENT LISTENERS **********
 //submit form
 form.addEventListener('submit', addItem);
+// clear items
+clearBtn.addEventListener('click', clearItems);
 // ****** FUNCTIONS **********
 function addItem(e){
     e.preventDefault();
@@ -42,6 +44,12 @@ function addItem(e){
         displayAlert('item added to the list', 'success');
         // show container
         container.classList.add('show-container');
+        // add to local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
+        
+
     }
     else if(value && editFlag){}
     else{
@@ -59,6 +67,30 @@ function displayAlert(text, action){
         alert.classList.remove(`alert-${action}`);
     },1000);
 }
+// clear items
+function clearItems(){
+    const items = document.querySelectorAll('.grocery-item');
+    if(items.length > 0){
+        items.forEach(function(item){
+            list.removeChild(item);
+        });
+    }
+    container.classList.remove("show-container");
+    displayAlert('empty list', 'danger');
+    setBackToDefault();
+    //localStorage.removeItem('list);
+}
+// set back to default
+function setBackToDefault(){
+    grocery.value = '';
+    editFlag = false;
+    editID = '';
+    submitBtn.textContent = "submit";
+}
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value){
+    console.log('added to local storage');
+}
+
 
 // ****** SETUP ITEMS **********
