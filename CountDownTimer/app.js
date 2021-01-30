81690781cf15a141c6dd0139fb3b9e449f04fd93
Solomon Asiedu <asiedusolo.sa@gmail.com>
2021-01-30 11:51:02 +0200

    dynamically adding the giveaway end date

--- CountDownTimer/app.js
+++ CountDownTimer/app.js
@@ -21,3 +21,22 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+// console.log(items);
+
+let futureDate = new Date(2021, 0, 30, 11, 27, 0);
+// console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const monthNumber = futureDate.getMonth();
+const dayOfWeekNumber = futureDate.getDay();
+let dayOfMonth = futureDate.getDate();
+console.log(dayOfMonth);
+
+
+giveaway.textContent = `giveaway ends on ${weekdays[dayOfWeekNumber]}, ${dayOfMonth} ${months[monthNumber]} ${year} ${hours}:${mins}am`;
